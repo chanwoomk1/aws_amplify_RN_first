5e009e8fd7f13d4c41c97ed2c077884a7d3d4587
chanwoomk1 <chanwooaws@gmail.com>
2025-01-15 13:45:34 +0000

    fixed for expo

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import {Text, View} from 'react-native';
 const HelloWorldApp = () => {
   return (
     <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-      <Text>Hello, world!</Text>
+      <Text>Hello, World!</Text>
     </View>
   );
 };
